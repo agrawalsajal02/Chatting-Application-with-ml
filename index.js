74b2791914aac7a11bf33091de770f93d0395813
SAJAL AGRAWAL <34278780+agrawalsajal02@users.noreply.github.com>
2020-03-14 01:34:20 +0530

    aa

--- index.js
+++ index.js
@@ -7,8 +7,15 @@ const request = require('request');
 
 app.get("/", function (req, res) {
     //res.send("hello there ");
-    var msg = `"ml chat application"|"sajal.agarwal703@gmail.com"|"8084561973"|"time abi"`;
+
+
+    var ip = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        (req.connection.socket ? req.connection.socket.remoteAddress : null);
+    var msg = `"ml chat application"|"sajal.agarwal703@gmail.com"|"8084561973"|"${ip}"`;
     var fullmsg = "https://www.fast2sms.com/dev/bulk?authorization=ejdkgTpc0ZuCsH5vL7OFSRwPyaG6UMQtWlYhnIXmqoA9f2xJb8LrCxtquT71KJegEi2NYVvcbRnkSowh&sender_id=FSTSMS&language=english&route=qt&numbers=" + "8084561973" + "&message=19086&variables={DD}|{EE}|{CC}|{FF}&variables_values=" + encodeURIComponent(msg);
+
     request.get(fullmsg, function (err, body) {
         if (err) {
             console.log("err in sending msg ");
